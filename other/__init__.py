e20e44fd3c419349d6e01af8c575fdee5091e474
Yevhen Kozlov <y@gmail.com>
2020-07-22 01:03:32 +0300

    Temp name generator is done!

--- other/__init__.py
+++ other/__init__.py
@@ -3,3 +3,4 @@ Initialization of this package
 """
 
 from .configs import MainConfig
+from .temp_name_generator import TempNameGenerator
---
+++ other/temp_name_generator.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+
+class TempNameGenerator:
+    """
+    Contain methods for generate random names
+    """
+
+    @staticmethod
+    def get_name(number_of_symbols: int) -> str:
+        """
+        Method for generate new random name
+
+        :return: str, new name
+        """
+
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for __ in range(number_of_symbols))
