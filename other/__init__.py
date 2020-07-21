ba5b5cb13211e924a01282e539a7e1f6f8ba636a
Yevhen Kozlov <y@gmail.com>
2020-07-22 01:23:58 +0300

    Options for imgkit

--- other/__init__.py
+++ other/__init__.py
@@ -4,3 +4,4 @@ Initialization of this package
 
 from .configs_wrapper import MainConfig
 from .temp_name_generator import TempNameGenerator
+from .imgkit_options import options
---
+++ other/imgkit_options.py
@@ -0,0 +1,8 @@
+"""
+This file contain options for imgkit
+"""
+
+options = {
+    'width': 1920,
+    'height': 1080,
+}
